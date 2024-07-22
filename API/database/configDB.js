a9036b727926801ca4826f025c3eb6b33c2b9e48
Koxtx <corentin.lavarde@gmail.com>
2024-07-22 14:57:47 +0200

    v2

---
+++ API/database/configDB.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoDb: {
+    uri: "mongodb+srv://corentinlavarde:HpUEUkKVYIYhgobb@cluster0.apacgk1.mongodb.net/Puissance4?retryWrites=true&w=majority&appName=Cluster0",
+  },
+};
