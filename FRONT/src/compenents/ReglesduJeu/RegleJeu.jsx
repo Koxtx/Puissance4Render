468ff1044a04c1f3ff7c0d55f859e5f0f57fab77
Koxtx <corentin.lavarde@gmail.com>
2024-07-26 10:42:16 +0200

    v9

--- FRONT/src/compenents/ReglesduJeu/RegleJeu.jsx
+++ FRONT/src/compenents/ReglesduJeu/RegleJeu.jsx
@@ -6,46 +6,38 @@ export default function RegleJeu() {
     <div className={`d-flex flex-column flex-fill center m-10 ${styles.regle}`}>
       <h1>Règles du jeu Puissance 4</h1>
       <p>
-        <strong>Objectif du jeu :</strong> Alignez 4 de vos jetons de couleur en
-        ligne, que ce soit horizontalement, verticalement ou en diagonale.
+        Le but du jeu consiste à disposer 4 de vos jetons de couleur en ligne,
+        que ce soit de manière horizontale, verticale ou diagonale.
       </p>
       <h2>Matériel :</h2>
       <ul>
         <li>Un plateau de jeu composé de 6 rangées et 7 colonnes.</li>
-        <li>
-          Deux couleurs de jetons (généralement rouge et jaune) en nombre
-          suffisant pour chaque joueur.
-        </li>
+        <li>Deux couleurs de jetons</li>
       </ul>
       <h2>Déroulement du jeu :</h2>
       <ol>
-        <li>Les joueurs choisissent chacun une couleur de jetons.</li>
         <li>
-          Les joueurs décident qui commence. Le jeu se déroule ensuite en
-          alternance.
+          À chaque tour, un joueur sélectionne une colonne et dépose un jeton
+          dans celle-ci. Le jeton descend jusqu'à la position la plus basse
+          offerte dans cette colonne.
         </li>
-        <li>
-          À chaque tour, un joueur choisit une colonne et place un jeton dans la
-          colonne choisie. Le jeton tombe jusqu'à la position la plus basse
-          disponible dans cette colonne.
-        </li>
-        <li>Le joueur suivant répète l'opération dans une autre colonne.</li>
+        <li>Le prochain joueur effectue la même opération.</li>
       </ol>
       <h2>Fin de la partie :</h2>
       <p>
-        La partie se termine lorsqu'un joueur parvient à aligner 4 jetons de sa
-        couleur en ligne (horizontalement, verticalement ou en diagonale) ou
-        lorsque toutes les colonnes sont pleines, ce qui entraîne une égalité.
+        La partie s'achève quand un joueur aligne 4 jetons de sa couleur en
+        ligne (horizontalement, verticalement ou en diagonale) ou quand toutes
+        les colonnes sont pleines, ce qui donne une égalité.
       </p>
       <h2>Règles supplémentaires :</h2>
       <ul>
         <li>
-          Les joueurs ne peuvent pas retirer de jetons une fois qu'ils ont été
-          placés dans le plateau.
+          Il est interdit aux joueurs de retirer des jetons une fois qu'ils ont
+          été placés sur le plateau.
         </li>
         <li>
-          En cas d'égalité, le jeu peut être recommencé ou les joueurs peuvent
-          convenir d'un autre résultat.
+          Si les joueurs sont égaux, le jeu peut être recommencé ou les joueurs
+          peuvent s'entendre sur un autre résultat.
         </li>
       </ul>
     </div>
