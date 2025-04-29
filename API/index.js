db3612b31b9df09000bf519049603bebf2bf06cd
corentin lavarde <161732983+Koxtx@users.noreply.github.com>
2025-04-29 11:52:05 +0200

    Update index.js

--- API/index.js
+++ API/index.js
@@ -10,7 +10,14 @@ const config = require("./database/configDB");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-type"],
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/games", gameRoutes);
